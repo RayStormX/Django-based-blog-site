a17eaa2905f6fe2b054c8f837e2e7eb429fa49a0
Ray Fernandez <raymond.r.fernandez@gmail.com>
2019-01-18 06:34:20 -0800

    added privacy policy page. blog is now main section of website (omitting 'blog/' in the url still redirects to blog section)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return HttpResponse('<h1>blog about page</h1>')
+
+def privacyPolicy(request):
+    return HttpResponse("you're not really going to read this")
