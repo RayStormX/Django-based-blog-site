67a5aed11789db53c7965c0baa460d3436290bc2
Ray Fernandez <raymond.r.fernandez@gmail.com>
2019-01-18 11:29:23 -0800

    changed h1 text in privacy policy

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
 from . import views
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
