0c4795b2697c16d3875a0431f5d99ebdee407094
Ray Fernandez <raymond.r.fernandez@gmail.com>
2019-01-18 09:09:04 -0800

    blog now has a couple example posts

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author':'ray',
+        'title':'blog post 1',
+        'content':'first post content',
+        'date_posted':'December 9, 1999'
+    },
+    {
+        'author':'ray',
+        'title':'blog post 2',
+        'content':'second post content',
+        'date_posted':'December 25, 1999'
+    }]
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
