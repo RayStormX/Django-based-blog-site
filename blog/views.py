16e9b5d95ce7e578706e8e815426e26ad73f6c64
Ray Fernandez <raymond.r.fernandez@gmail.com>
2019-01-19 13:19:06 -0800

    added view, model, and template for individual blog posts.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 # Create your views here.
 """
+dummy data
+
 posts=[
     {
         'author':'ray',
@@ -16,7 +19,26 @@ posts=[
         'content':'second post content',
         'date_posted':'December 25, 1999'
     }]
+
 """
+
+"""
+class-based views
+"""
+class PostListView(ListView):
+    """
+    tells our ListView what model to query in order to create the list
+    """
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+
 def home(request):
     context = {
         'posts': Post.objects.all()
