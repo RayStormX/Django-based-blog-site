478b0207a8b2010e3a843212f0a3ab3edde6837f
Ray Fernandez <raymond.r.fernandez@gmail.com>
2019-01-20 03:23:31 -0800

    gitignore

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import(
     ListView,
     DetailView,
@@ -24,9 +25,12 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     fields =['title','content']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def home(request):
