29e0d48904c3a03074d1177f1e8ccbcd390f87eb
Ray Fernandez <raymond.r.fernandez@gmail.com>
2019-01-20 03:23:31 -0800

    gitignore

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,7 @@ class Post(models.Model):
         
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk':self.pk})
+
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk': self.pk})
+        
